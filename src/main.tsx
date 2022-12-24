5724fbf367f1cbfe7c62dcf15789beb012b727ce
silnex <silnex@silnex.kr>
2022-12-24 15:02:59 +0900

    Add redux devtools

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,11 @@ import { createStore } from "redux";
 import App from "./App";
 import rootReducer from "./modules";
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
